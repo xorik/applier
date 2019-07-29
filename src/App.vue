654057ecb69f0b2941e8cd9d283eae2d70ac3fec
 Andrey Smelov <xor29a@bk.ru>
2019-07-29 15:26:09 +0300

    Pug, bootstrap, fontawesome

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<template>
-  <div id="app"></div>
+<template lang="pug">
+  .container
+    h3 yo!!
+    button.btn.btn-primary
+      fa(icon="plus")
+      |
+      | Plus
 </template>
 
 <script lang="ts">
@@ -8,3 +13,7 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component({})
 export default class App extends Vue {}
 </script>
+
+<style lang="scss">
+@import '~bootstrap/scss/bootstrap';
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import Vue, { CreateElement, VNode } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
+import './icons'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.component('fa', FontAwesomeIcon)
 
 new Vue({
   render: (h: CreateElement): VNode => h(App),
